0171377142a5236910185fe99fcc7ac780e89540
song4529 <gusdn4529@gmail.com>
2024-03-24 20:54:58 +0900

    3개 중에서 1개를 뽑는 행위를 80번 할 때 특정한 1개를 뽑는데 성공한 횟수를 표현하는 시뮬레이션

---
+++ src/main/java/com/study/algorithm/RandomPick.java
@@ -0,0 +1,42 @@
+package com.study.algorithm;
+import java.util.Random;
+
+public class RandomPick {
+    public static void main(String[] args) {
+        int numSimulations = 1000000;
+        int successes = countSuccesses(numSimulations);
+        System.out.println("성공한 횟수 : " + successes);
+    }
+
+    public static boolean simulateTrial() {
+        int countA = 0;
+        Random random = new Random();
+        for (int i = 0; i < 80; i++) {
+            String result = getRandomResult(random);
+            if (result.equals("a")) {
+                countA++;
+                if (countA >= 35) {
+
+                    return true; // 'a'를 30번 이상 획득하여 종료
+                }
+            }
+        }
+        return false; // 'a'를 30번 이상 획득하지 못하고 종료
+    }
+
+    public static int countSuccesses(int numSimulations) {
+        int successCount = 0;
+        for (int i = 0; i < numSimulations; i++) {
+            if (simulateTrial()) {
+                successCount++;
+            }
+        }
+        return successCount;
+    }
+
+    public static String getRandomResult(Random random) {
+        String[] options = {"a", "b", "c"};
+        int index = random.nextInt(options.length);
+        return options[index];
+    }
+}
