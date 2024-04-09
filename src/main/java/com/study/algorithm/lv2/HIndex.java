27d6c9ef8dc6aee89ca4dec4339b728f6f55ba72
song4529 <gusdn4529@gmail.com>
2024-04-09 23:53:44 +0900

    H-Index 프로그래머스

---
+++ src/main/java/com/study/algorithm/lv2/HIndex.java
@@ -0,0 +1,22 @@
+package com.study.algorithm.lv2;
+
+import java.util.*;
+
+class HIndex {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for(int i = 0; i < citations.length; i++) {
+            int h = citations.length - i; // 인용된 논문의 수
+
+            if(citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
