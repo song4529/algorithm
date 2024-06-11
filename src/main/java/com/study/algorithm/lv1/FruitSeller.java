0938d0fcc2eafa68c9752ad5e9c14dd1bc66aede
song4529 <gusdn4529@gmail.com>
2024-06-11 23:38:53 +0900

    과일장수 프로그래머스

---
+++ src/main/java/com/study/algorithm/lv1/FruitSeller.java
@@ -0,0 +1,17 @@
+package com.study.algorithm.lv1;
+
+import java.util.*;
+
+class FruitSeller {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
+        }
+
+        return answer;
+    }
+}
