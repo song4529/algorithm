8161b516f4f213b2accc40a5e5109ad3049045b8
song4529 <gusdn4529@gmail.com>
2024-06-19 23:52:39 +0900

    k번째 숫자 프로그래머스

---
+++ src/main/java/com/study/algorithm/lv1/KthNumber.java
@@ -0,0 +1,17 @@
+package com.study.algorithm.lv1;
+
+
+import java.util.Arrays;
+
+class KthNumber {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
