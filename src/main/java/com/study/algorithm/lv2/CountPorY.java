ac91ff50c619e5d28261e34129a43ce9b66f7d95
song4529 <gusdn4529@gmail.com>
2024-10-24 23:24:48 +0900

    문자열 내 p와 y의 개수 프로그래머스

---
+++ src/main/java/com/study/algorithm/lv2/CountPorY.java
@@ -0,0 +1,18 @@
+package com.study.algorithm.lv2;
+
+class CountPorY {
+    boolean solution(String s) {
+        s = s.toLowerCase();
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == 'p')
+                count++;
+            else if (s.charAt(i) == 'y')
+                count--;
+        }
+
+        return count == 0;
+    }
+}
