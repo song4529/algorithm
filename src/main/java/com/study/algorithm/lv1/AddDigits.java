1e5397c0e1d70950e5260e491dfcf7225e29d8c7
song4529 <gusdn4529@gmail.com>
2024-10-26 23:59:56 +0900

    자릿수 더하기 프로그래머스

---
+++ src/main/java/com/study/algorithm/lv1/AddDigits.java
@@ -0,0 +1,22 @@
+package com.study.algorithm.lv1;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        while(true){
+            answer+=n%10;
+            if(n<10)
+                break;
+
+            n=n/10;
+        }
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
