29e23e3a2a735af072800813a7b62e7462249ca4
song4529 <gusdn4529@gmail.com>
2024-10-29 22:58:34 +0900

    더 맵게 프로그래머스

---
+++ src/main/java/com/study/algorithm/lv2/MoreSpicy.java
@@ -0,0 +1,20 @@
+package com.study.algorithm.lv2;
+
+import java.util.PriorityQueue;
+class MoreSpicy {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            que.add(scoville[i]);
+        }
+        while(que.peek() < K) {
+            if (que.size() == 1)
+                return -1;
+            que.add(que.poll() + que.poll() * 2);
+            answer++;
+        }
+        return answer;
+    }
+}
