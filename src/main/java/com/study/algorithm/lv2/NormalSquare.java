026e64917da848ead5fe6f31e3121cf03d10e2f8
song4529 <gusdn4529@gmail.com>
2024-11-04 23:44:39 +0900

    멀쩡한 사각형 프로그래머스

---
+++ src/main/java/com/study/algorithm/lv2/NormalSquare.java
@@ -0,0 +1,16 @@
+package com.study.algorithm.lv2;
+
+
+class NormalSquare {
+    public long solution(int w, int h) {
+        long answer = 0;
+        long x = (long)w;
+        long y = (long)h;
+
+        for(int i=1;i<w;i++){
+            answer += y*i/x ;
+        }
+
+        return answer*2;
+    }
+}
